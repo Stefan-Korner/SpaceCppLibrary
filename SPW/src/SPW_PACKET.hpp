cce95851c6e4fb0ce744e85eab6e59ee911091fe
Stefan Korner <korner-hajek@gmx.at>
2014-04-06 23:00:18 +0200

    prepare the header file for SpaceWire and RMAP

--- SPW/src/SPW_PACKET.hpp
+++ SPW/src/SPW_PACKET.hpp
@@ -22,6 +22,15 @@ namespace SPW
 {
   namespace PACKET
   {
+    ///////////////
+    // constants //
+    ///////////////
+    static const uint8_t EXTENDED_PROTOCOL_ID = 0;
+    static const uint8_t RMAP_PROTOCOL_ID = 1;
+    static const uint8_t CCSDS_PROTOCOL_ID = 2;
+    static const uint8_t GOES_PROTOCOL_ID = 238;
+    static const uint8_t STUP_PROTOCOL_ID = 239;
+
     //-------------------------------------------------------------------------
     class Packet: public UTIL::DU
     //-------------------------------------------------------------------------
@@ -29,12 +38,127 @@ namespace SPW
     public:
       // constructors and destructur
       Packet();
-      Packet(size_t p_bufferSize);
-      Packet(void* p_buffer, size_t p_bufferSize);
-      Packet(const void* p_buffer, size_t p_bufferSize, bool p_copyBuffer);
+      Packet(size_t p_spwAddrSize, size_t p_spwDataSize);
+      Packet(void* p_buffer, size_t p_bufferSize, size_t p_spwAddrSize);
+      Packet(const void* p_buffer,
+             size_t p_bufferSize,
+             bool p_copyBuffer,
+             size_t p_spwAddrSize);
       Packet(const Packet& p_du);
       const Packet& operator=(const Packet& p_du);
       virtual ~Packet();
+
+      // access methods
+      virtual size_t getSPWaddrSize() const throw(UTIL::Exception);
+      virtual void setSPWaddr(size_t p_byteLength, const void* p_bytes)
+        throw(UTIL::Exception);
+      virtual const uint8_t* getSPWaddr() const throw(UTIL::Exception);
+      virtual void setLogAddr(uint8_t p_logAddr) throw(UTIL::Exception);
+      virtual uint8_t getLogAddr() const throw(UTIL::Exception);
+      virtual void setProtocolID(uint8_t p_id) throw(UTIL::Exception);
+      virtual uint8_t setProtocolID() const throw(UTIL::Exception);
+      virtual size_t getSPWdataSize() const throw(UTIL::Exception);
+      virtual void setSPWdata(size_t p_byteLength, const void* p_bytes)
+        throw(UTIL::Exception);
+      virtual const uint8_t* getSPWdata() const throw(UTIL::Exception);
+
+    private:
+      size_t m_spwAddrSize;
+    };
+
+    //-------------------------------------------------------------------------
+    class RMAPpacket: public Packet
+    //-------------------------------------------------------------------------
+    {
+    public:
+      // constructors and destructur
+      RMAPpacket();
+      RMAPpacket(size_t p_spwAddrSize,
+                 uint8_t p_instruction,
+                 size_t p_dataSize);
+      RMAPpacket(void* p_buffer, size_t p_bufferSize, size_t p_spwAddrSize);
+      RMAPpacket(const void* p_buffer,
+                 size_t p_bufferSize,
+                 bool p_copyBuffer,
+                 size_t p_spwAddrSize);
+      RMAPpacket(const RMAPpacket& p_du);
+      const RMAPpacket& operator=(const RMAPpacket& p_du);
+      virtual ~RMAPpacket();
+
+      // access methods
+      virtual uint8_t getInstruction() const throw(UTIL::Exception);
+      virtual void setHeaderCRC() throw(UTIL::Exception);
+      virtual uint8_t getHeaderCRC() const throw(UTIL::Exception);
+      virtual size_t getDataSize() const throw(UTIL::Exception);
+      virtual uint32_t getDataLength() const throw(UTIL::Exception);
+      virtual void setData(size_t p_byteLength, const void* p_bytes)
+        throw(UTIL::Exception);
+      virtual const uint8_t* getData() const throw(UTIL::Exception);
+      virtual void setDataCRC() throw(UTIL::Exception);
+      virtual uint8_t getDataCRC() const throw(UTIL::Exception);
+    };
+
+    //-------------------------------------------------------------------------
+    class RMAPcommand: public RMAPpacket
+    //-------------------------------------------------------------------------
+    {
+    public:
+      // constructors and destructur
+      RMAPcommand();
+      RMAPcommand(size_t p_targetSPWaddrSize,
+                  uint8_t p_instruction,
+                  size_t p_dataSize);
+      RMAPcommand(void* p_buffer, size_t p_bufferSize, size_t p_targetSPWaddrSize);
+      RMAPcommand(const void* p_buffer,
+                  size_t p_bufferSize,
+                  bool p_copyBuffer,
+                  size_t p_targetSPWaddrSize);
+      RMAPcommand(const RMAPcommand& p_du);
+      const RMAPcommand& operator=(const RMAPcommand& p_du);
+      virtual ~RMAPcommand();
+
+      // access methods
+      virtual void setReplyAddr(size_t p_byteLength, const void* p_bytes)
+        throw(UTIL::Exception);
+      virtual const uint8_t* getReplyAddr() const throw(UTIL::Exception);
+      virtual void setInitLogAddr(uint8_t p_logAddr) throw(UTIL::Exception);
+      virtual uint8_t getInitLogAddr() const throw(UTIL::Exception);
+      virtual void setTransactionID(uint16_t p_transID) throw(UTIL::Exception);
+      virtual uint16_t getTransactionID() const throw(UTIL::Exception);
+      virtual void setExtendedMemAddr(uint8_t p_extMemAddr) throw(UTIL::Exception);
+      virtual uint8_t getExtendedMemAddr() const throw(UTIL::Exception);
+      virtual void setMemoryAddr(uint32_t p_memAddr) throw(UTIL::Exception);
+      virtual uint32_t getMemoryAddr() const throw(UTIL::Exception);
+      virtual void setReadModWriteData(uint32_t p_data) throw(UTIL::Exception);
+      virtual uint32_t getReadModWriteData() const throw(UTIL::Exception);
+      virtual void setReadModWriteMask(uint32_t p_mask) throw(UTIL::Exception);
+      virtual uint32_t getReadModWriteMask() const throw(UTIL::Exception);
+    };
+
+    //-------------------------------------------------------------------------
+    class RMAPreply: public RMAPpacket
+    //-------------------------------------------------------------------------
+    {
+    public:
+      // constructors and destructur
+      RMAPreply();
+      RMAPreply(const RMAPcommand& p_command);
+      RMAPreply(void* p_buffer, size_t p_bufferSize, size_t p_initSPWaddrSize);
+      RMAPreply(const void* p_buffer,
+                size_t p_bufferSize,
+                bool p_copyBuffer,
+                size_t p_initSPWaddrSize);
+      RMAPreply(const RMAPreply& p_du);
+      const RMAPreply& operator=(const RMAPreply& p_du);
+      virtual ~RMAPreply();
+
+      // access methods
+      virtual void setStatus(uint8_t p_status) throw(UTIL::Exception);
+      virtual uint8_t getStatus() const throw(UTIL::Exception);
+      virtual uint8_t getInitLogAddr() const throw(UTIL::Exception);
+      virtual uint16_t getTransactionID() const throw(UTIL::Exception);
+      virtual void setReadModWriteData(uint32_t p_data) throw(UTIL::Exception);
+      virtual uint32_t getReadModWriteData() const throw(UTIL::Exception);
     };
   }
 }
